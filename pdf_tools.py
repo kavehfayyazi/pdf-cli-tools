f41067f9e6491149ed92f00604f0677fd20501f4
Kaveh <kfayy000@gmail.com>
2025-07-16 01:13:14 -0400

    Create utils.py

--- pdf_tools.py
+++ pdf_tools.py
@@ -2,6 +2,7 @@ import argparse
 import sys
 import os
 from pikepdf import Pdf as PDF
+from utils import validate_file_exists, get_valid_filename, expand_pages
 
 # pdf-tools merge <path1> <path2> [-p1 <pages1> | -pages1 <pages1>] [-p2 <pages2> | --pages2 <pages2>] [-o <out>]
 def merge():
@@ -19,58 +20,38 @@ def merge():
     merged = PDF.new()
 
     # Error checking nonexistent PDFs
-    for path in [args.path1, args.path2]:
-        if not os.path.isfile(os.path.join(os.getcwd(), path)):
-            print(f"Error: File not found: {path}", file=sys.stderr)
-            return 
+    validate_file_exists(args.path1)
+    validate_file_exists(args.path2)
         
     for path, pages in [(args.path1, args.pages1), (args.path2, args.pages2)]:
         with PDF.open(path) as pdf:
-            if pages == 'all':
-                for i in range(len(pdf.pages)):
-                    merged.pages.append(pdf.pages[i])
-            else:
-                page_list = []
-                page_nums = pages.split(',')
-                for page_range in page_nums:
-                    # change any if they have a colon
-                    if ':' in page_range:
-                        left, right = page_range.split(':')
-                        for num in range(int(left), int(right) + 1):
-                            page_list.append(num)
-                    else:
-                        page_list.append(page_range)
-                for page_num in page_list:
-                    # Error checking for invalid page numbers
-                    if int(page_num) < 1 or int(page_num) > len(pdf.pages):
-                        print(f"Error: Invalid page number ({path}): {page_num}", file=sys.stderr)
-                        return
-                    merged.pages.append(pdf.pages[int(page_num) - 1])
+            for page_num in expand_pages(pdf, pages):
+                merged.pages.append(pdf.pages[page_num])
 
-    # File Name
-    current_dir = os.getcwd()
-    filename = ''
-    i = 0
-    while True:
-        filename = args.out if i == 0 else f'{args.out}{i}.pdf'
-        filepath = os.path.join(current_dir, filename)
-        if os.path.isfile(filepath):
-            i += 1
-            continue
-        break
-
+    filename = get_valid_filename(args.out)
     merged.save(filename)
 
 # pdf-tools rotate <path> [-p <pages | --pages <pages>] [-a deg | --angle deg] [-o <out>]
 def rotate():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description='Rotate a PDF',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
     parser.add_argument('path',             help='file path of the PDF',                            type=str)
     parser.add_argument('-p', '--pages',  help='selected pages of the PDF',                       type=str)
     parser.add_argument('-a', '--angle',    help='clockwise degrees of rotation (e.g. 90,180,270)', type=int, default=90)
     parser.add_argument('-o', '--out',      help='file name of the output PDF',                     type=str, default='rotated.pdf')
     args = parser.parse_args(sys.argv[2:])
 
-    print(args)
+    rotated = PDF.new()
+
+    # Error checking nonexistent PDF
+    validate_file_exists(args.path)
+        
+    
+    
+    filename = get_valid_filename(args.out)
+    rotated.save(filename)
 
 # pdf-tools reverse <path> [-p <pages | --pages <pages>] [-o <out>]
 def reverse():
---
+++ utils.py
@@ -0,0 +1,53 @@
+import sys
+import os
+from pikepdf import Pdf as PDF
+
+def validate_file_exists(path: str):
+    if not os.path.isfile(os.path.join(os.getcwd(), path)):
+        print(f"Error: File not found: {path}", file=sys.stderr)
+        sys.exit(1)
+
+def get_valid_filename(filename: str) -> str:
+    current_dir = os.getcwd()
+    valid_name = ''
+    i = 0
+    while True:
+        valid_name = filename if i == 0 else f'{filename.split('.')[0]}{i}.pdf'
+        filepath = os.path.join(current_dir, valid_name)
+        if os.path.isfile(filepath):
+            i += 1
+            continue
+        break
+    return valid_name
+
+""" all -> 0 , len(pdf.pages) - 1
+
+for customs,
+it subtracts one to account for pages.pdf starting at 0
+
+"""
+def expand_pages(pdf: PDF, pages: str) -> list:
+    if pages == 'all':
+        return [x for x in range(len(pdf.pages))]
+    else:
+        page_list = []
+        page_nums = pages.split(',')
+        for page_range in page_nums:
+            if ':' in page_range:
+                parts = page_range.split(':')
+                if len(parts) != 2 or not all(part.isdigit() for part in parts):
+                    print(f"Error: Invalid page number: {page_range}", file=sys.stderr)
+                    sys.exit(1)
+                left, right = map(int, parts)
+                for num in range(left - 1, right):
+                    page_list.append(num)
+            else:
+                if not page_range.isdigit():
+                    print(f"Error: Invalid page number: {page_range}", file=sys.stderr)
+                    sys.exit(1)
+                page_list.append(int(page_range) - 1)
+        for page_num in page_list:
+            if page_num < 0 or page_num >= len(pdf.pages):
+                print(f"Error: Invalid page number: {page_num + 1}", file=sys.stderr)
+                sys.exit(1)
+        return page_list
